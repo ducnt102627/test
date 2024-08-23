e4eca086e9d2db70bf7e5bf8a56f6416afb77ae2
ducnt27 <ducnt2726@gmail.com>
2024-08-23 21:59:11 +0700

    Update v2

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -17,15 +17,7 @@ interface AuthProviderProps {
 const AuthProvider = ({ children }: AuthProviderProps) => {
   const [authUser, setAuthUser] = useState<IUser | undefined>(undefined);
   const [isSignIn, setIsSignIn] = useState(Boolean(localStorage.getItem('accessToken') && localStorage.getItem('accessToken')));
-  // console.log("Sinin", isSignIn)
-  // useEffect(() => {
-  //   const token = localStorage.getItem('accessToken');
-  //   console.log(token);
-  //   if (token) {
-  //     setIsSignIn(true);
-  //     // setAuthUser({ username: 'John Doe' });
-  //   }
-  // });
+
   return <AuthContext.Provider value={{ authUser, setAuthUser, isSignIn, setIsSignIn }}>{children}</AuthContext.Provider>;
 }
 export { AuthContext, AuthProvider };
